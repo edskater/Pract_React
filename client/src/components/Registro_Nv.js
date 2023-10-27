b1c2e5aca3966fbf4853fe533192286bd2065c5c
ED_dev <eduardo.varelas@durango.gob.mx>
2023-10-27 09:49:08 -0600

    Registro final

--- client/src/components/Registro_Nv.js
+++ client/src/components/Registro_Nv.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextField, Select, MenuItem, FormControl, InputLabel, Switch, FormControlLabel, Box, Container, Grid,  } from '@mui/material';
+import { TextField, Select, MenuItem, FormControl, InputLabel, Switch, FormControlLabel, Box, Container, Grid, Typography, Button, ButtonGroup } from '@mui/material';
 import { useState } from 'react';
 
 
@@ -44,107 +44,114 @@ export default function RegistrationForm() {
     const curp = event.target.value.toUpperCase();
     setCurpValidity(curpValida(curp));
   };
+
+
  
 
 
   return (
     
-        <Container>
-                <Box height={'74vh'} flexDirection="column"  display={'flex'} > 
+      <Container>
+        <Typography variant="h3" gutterBottom>
+         Datos Generales
+        </Typography>
 
-                    <Grid container spacing={2} >
-                        <Grid item xs={4} >
-                            <TextField fullWidth required label="Nombre(s)" />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <TextField fullWidth required label="Primer Apellido" />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <TextField fullWidth label="Segundo Apellido" />
-                        </Grid>
-                    </Grid>
+              <Box height={'668px'} flexDirection="column" justifyContent={'space-evenly'} display={'flex'}>
 
-                    <Grid container spacing={2}>
-                        <Grid item xs={2}>
-                            <TextField fullWidth required type="date" label="Fecha de Nacimiento"  InputLabelProps={{ shrink: true }} />
-                        </Grid>
-                        
-                        <Grid item xs={2}>
-                                <FormControl fullWidth required>
-                                    <InputLabel>Sexo</InputLabel>
-                                    <Select sx={{ minWidth: '92px'}} >
-                                    {/* Aquí puedes añadir tus opciones */}
-                                    <MenuItem value="Option 1">Option 1</MenuItem>
-                                    <MenuItem value="Option 2">Option 2</MenuItem>
-                                    </Select>
-                                </FormControl> 
-                        </Grid>
-                        <Grid item xs={4}>
-                            <FormControl fullWidth required >
-                                        <InputLabel>Estado de Nacimiento</InputLabel>
-                                        <Select>
-                                        {/* Aquí puedes añadir tus opciones */}
-                                        <MenuItem value="Option 1">Option 1</MenuItem>
-                                        <MenuItem value="Option 2">Option 2</MenuItem>
-                                        </Select>   
-                                    </FormControl>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <TextField  required label="CURP"
-                                        id="curpInput" /* Asigna un id al campo de CURP */
-                                        onChange={handleCurpChange} /*  Agrega el manejador de cambios */
-                                        error={!curpValidity} /*  Muestra un error si la CURP no es válida */
-                                        helperText={!curpValidity ? "CURP no válida" : ""} /* Mensaje de error */
-                                        fullWidth />
-                        </Grid> 
-                    </Grid>  
+                  <Grid container spacing={2}>
+                      <Grid item xs={4}>
+                          <TextField fullWidth required label="Nombre(s)" />
+                      </Grid>
+                      <Grid item xs={4}>
+                          <TextField fullWidth required label="Primer Apellido" />
+                      </Grid>
+                      <Grid item xs={4}>
+                          <TextField fullWidth label="Segundo Apellido" />
+                      </Grid>
+                  </Grid>
 
-                    <Grid container spacing={2}>
-                        <Grid item xs={4}>
-                            <FormControl fullWidth required>
-                                <InputLabel>ocupacion</InputLabel>
-                                    <Select sx={{ minWidth: '132px'}}>
-                                    {/* Aquí puedes añadir tus opciones */}
-                                        <MenuItem value="Option 1">Option 1</MenuItem>
-                                        <MenuItem value="Option 2">Option 2</MenuItem>
-                                    </Select>
-                            </FormControl>
-                        </Grid>
+                  <Grid container spacing={2}>
+                      <Grid item xs={2}>
+                          <TextField fullWidth required type="date" label="Fecha de Nacimiento" InputLabelProps={{ shrink: true }} />
+                      </Grid>
 
-                        <Grid item xs={4}>
-                            <TextField  fullWidth required label="Correo Electronico" />
-                        </Grid>
-        
-                        <Grid item xs={4}>
-                            <TextField fullWidth required label="Telefono" />
-                        </Grid>
-                    </Grid>
-                                         
-                    <FormControlLabel
-                        control={
-                        <Switch
-                            checked={emergencyContact}
-                            onChange={handleSwitchChange}
-                            name="emergencyContactSwitch"
-                            color="primary"
-                        />
-                        }
-                        label="Contacto para llamar en caso de emergencia"
-                    />
-                    {emergencyContact && (
-                        <>
-                        <Grid container spacing={2}>
-                            <Grid item xs={6}>
-                                <TextField fullWidth required label="Nombre completo" inputProps={{ maxLength: 50 }} />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField fullWidth required label="Teléfono" type="tel" inputProps={{ maxLength: 10 }} />
-                            </Grid>
-                        </Grid>
-                        </>
-                    )}
-                </Box>                
-        </Container>    
+                      <Grid item xs={2}>
+                          <FormControl fullWidth required>
+                              <InputLabel>Sexo</InputLabel>
+                              <Select sx={{ minWidth: '92px' }}>
+                                  {/* Aquí puedes añadir tus opciones */}
+                                  <MenuItem value="Option 1">Option 1</MenuItem>
+                                  <MenuItem value="Option 2">Option 2</MenuItem>
+                              </Select>
+                          </FormControl>
+                      </Grid>
+                      <Grid item xs={4}>
+                          <FormControl fullWidth required>
+                              <InputLabel>Estado de Nacimiento</InputLabel>
+                              <Select>
+                                  {/* Aquí puedes añadir tus opciones */}
+                                  <MenuItem value="Option 1">Option 1</MenuItem>
+                                  <MenuItem value="Option 2">Option 2</MenuItem>
+                              </Select>
+                          </FormControl>
+                      </Grid>
+                      <Grid item xs={4}>
+                          <TextField required label="CURP"
+                              id="curpInput" /* Asigna un id al campo de CURP */
+                              onChange={handleCurpChange} /*  Agrega el manejador de cambios */
+                              error={!curpValidity} /*  Muestra un error si la CURP no es válida */
+                              helperText={!curpValidity ? "CURP no válida" : ""} /* Mensaje de error */
+                              fullWidth />
+                      </Grid>
+                  </Grid>
+
+                  <Grid container spacing={2}>
+                      <Grid item xs={4}>
+                          <FormControl fullWidth required>
+                              <InputLabel>ocupacion</InputLabel>
+                              <Select sx={{ minWidth: '132px' }}>
+                                  {/* Aquí puedes añadir tus opciones */}
+                                  <MenuItem value="Option 1">Option 1</MenuItem>
+                                  <MenuItem value="Option 2">Option 2</MenuItem>
+                              </Select>
+                          </FormControl>
+                      </Grid>
+
+                      <Grid item xs={4}>
+                          <TextField fullWidth required label="Correo Electronico" />
+                      </Grid>
+
+                      <Grid item xs={4}>
+                          <TextField fullWidth required label="Telefono" />
+                      </Grid>
+                  </Grid>
+
+                  <FormControlLabel
+                      control={<Switch
+                          checked={emergencyContact}
+                          onChange={handleSwitchChange}
+                          name="emergencyContactSwitch"
+                          color="primary" />}
+                      label="Contacto para llamar en caso de emergencia" />
+                  {emergencyContact && (
+                      <>
+                          <Grid container spacing={2}>
+                              <Grid item xs={6}>
+                                  <TextField fullWidth required label="Nombre completo" inputProps={{ maxLength: 50 }} />
+                              </Grid>
+                              <Grid item xs={6}>
+                                  <TextField fullWidth required label="Teléfono" type="tel" inputProps={{ maxLength: 10 }} />
+                              </Grid>
+                          </Grid>
+                      </>
+                  )}
+
+                    <ButtonGroup variant="text" aria-label=" outlined large button group" style={{justifyContent:'flex-end'}} size="large">
+                        <Button key="Atras">Atras</Button>
+                        <Button key="Siguiente">Siguiente</Button>
+                    </ButtonGroup>
+              </Box>
+          </Container>   
   );
 }
 
